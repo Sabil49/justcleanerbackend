3b36e2da634434bdc35706f030929b2b93ac9842
Sabil49 <md.sabeel10@gmail.com>
2025-11-04 23:22:45 +0530

    change time

--- app/api/cron/daily-reminders/route.ts
+++ app/api/cron/daily-reminders/route.ts
@@ -20,7 +20,7 @@ export async function GET(request: NextRequest) {
       where: {
         isPremium: false,
         updatedAt: {
-          lt: oneDayAgo,
+          gt: oneDayAgo,
         },
       },
       select: {
